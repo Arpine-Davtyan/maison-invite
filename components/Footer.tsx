7c14e52abe5bb997d97d746e3e2c1a9c20946f3a
Arpine-Davtyan <arpinedavtyan5@gmail.com>
2026-08-24 15:04:24 +0400

    Footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <footer>
-            <div className="container justify-between">
+            <div className="max-w-7xl w-full mx-auto flex max-sm:flex-col max-sm:text-center items-center sm:justify-between">
                 <h5>Maison Invite</h5>
                 <p className="flex items-center text-sm text-wenge">Digital invitations, delivered as a link. Made to be opened.</p>
             </div>
